9caaf57cc6831cf2ef129f96293707e0d5965cb0
fujivara <marat.skyba@gmail.com>
2024-02-16 18:28:41 +0200

    Added error handler

--- app.go
+++ app.go
@@ -15,7 +15,10 @@ func fooHandler(w http.ResponseWriter, r *http.Request) {
 	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		return
+	}
 }
 
 func main() {
